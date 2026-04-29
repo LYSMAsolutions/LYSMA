0fb152397fc34aa02859e9ac35c0294cb849288f
LYSMA solutions <mathieu0287@gmail.com>
2026-04-29 15:48:09 +0200

    Initial commit LYSMA

---
+++ packages/db/src/client.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "./generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL manquante au runtime.");
+}
+
+const globalForPrisma = global as unknown as {
+  prisma?: PrismaClient;
+};
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+    log: ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
